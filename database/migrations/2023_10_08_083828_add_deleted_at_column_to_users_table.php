e2d3ec107505acb0a429eb9becf18ad01f927057
Kyohei1230 <iritsukyohei@gmail.com>
2023-10-09 09:40:01 +0900

    usersマイグレーション

--- database/migrations/2023_10_08_083828_add_deleted_at_column_to_users_table.php
+++ database/migrations/2023_10_08_083828_add_deleted_at_column_to_users_table.php
@@ -13,9 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes()->nullable();
         });
     }
 
@@ -26,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
